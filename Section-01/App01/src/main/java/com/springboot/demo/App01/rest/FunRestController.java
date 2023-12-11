dfebc4659c87d604995ea7a6a3fe6bd8a0f8aa99
rameez.khalil <rameezkhalil23@gmail.com>
2023-12-11 15:26:52 -0800

    section1

--- Section-01/App01/src/main/java/com/springboot/demo/App01/rest/FunRestController.java
+++ Section-01/App01/src/main/java/com/springboot/demo/App01/rest/FunRestController.java
@@ -1,14 +1,23 @@
 package com.springboot.demo.App01.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${best.name}")
+    private String name;
+
 
     @GetMapping("/") //exposing route "/"
     public String hello() {
-        return "Hello Rameez, you are the best";
+        return "Hello  " + name + " , you are the besta";
+    }
+
+    @GetMapping("/hellotest")
+    public String test(){
+        return "Just testingssa";
     }
 }
